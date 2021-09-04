c673842fb6a3db94bfaff168066ec546c41177e0
suabae <bsa0322@ewhain.net>
2021-09-04 20:45:41 +0900

    [맵과 셋] 9월 4일 (과제 2개)

---
+++ 9월 7일 - 맵과 셋/14425.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+/*
+ * set::find 사용 문제
+ * set::find 함수는 찾는 key 값이 존재한다면 해당 iterator를 반환
+ * 따라서 찾는 key 값이 없다면 set::end를 반환
+ */
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    set<string> s;
+    string str;
+    int n, m, ans = 0;
+
+    //입력
+    cin >> n >> m;
+
+    //집합 s에 포함되어 있는 문자열 입력
+    for (int i = 0; i < n; i++) {
+        cin >> str;
+        s.insert(str);
+    }
+
+    //검사해야 하는 문자열 입력
+    for (int i = 0; i < m; i++) {
+        cin >> str;
+        //집합 s에 포함되어 있다면 ans 증가
+        if (s.find(str) != s.end()) ans++;
+    }
+    cout << ans << '\n';
+    return 0;
+}
