a3c05d6d731c591f73b4744340842429637c5424
suabae <bsa0322@ewhain.net>
2021-09-05 23:16:06 +0900

    [구현&코너케이서-2] 9월 5일 - Update

--- 9월 7일 - 맵과 셋/10757.cpp
+++ 9월 7일 - 맵과 셋/10757.cpp
@@ -13,33 +13,35 @@ using namespace std;
  * 배열을 사용할 경우 마지막 인덱스부터 출력하면 됨
  */
 
-//한 자릿수씩 더해서 값 리턴하는 함수
-int digitPlus(string &a, string &b, int p1, int p2, bool carry) {
-    int num = 0;
-    if (p1 >= 0) //p1 인덱스가 0 이상이라면 -> a에 남은 수가 존재한다면
-        num += (a[p1] - '0');
-    if (p2 >= 0) //p2 인덱스가 0 이상이라면 -> b에 남은 수가 존재한다면
-        num += (b[p2] - '0');
-    if (carry) //전 자릿수에서 올림되었다면
-        return num + 1;
-    return num;
-}
-
 //A + B 함수, 일의 자릿수부터 더함
 stack<int> calcPlus(string &a, string &b) {
     stack<int> st;
     int p1 = a.length() - 1; //a의 일의 자릿수 인덱스
     int p2 = b.length() - 1; //b의 일의 자릿수 인덱스
-    int num = 0;
+    bool carry = false; //올림 체크 변수
 
-    while (p1 >= 0 || p2 >= 0) { //자릿수 더하기
-        if (num >= 10) //그 전 자릿수 더하기 값이 10을 넘었다면 올림변수인 carry를 true로 호출
-            num = digitPlus(a, b, p1--, p2--, true);
-        else
-            num = digitPlus(a, b, p1--, p2--, false);
-        st.push(num % 10); //올림값 제외한 한 자릿수만 스택에 저장
+    while (p1 >= 0 && p2 >= 0) { //자릿수 더하기
+        int num = (a[p1--] - '0') + (b[p2--] - '0'); //a와 b의 각 자릿수 값 더하기
+        if (carry) //전 자릿수에서 올림되었다면
+            num += 1;
+        carry = num / 10;  //자릿수 더한 값이 10을 넘으면 올림변수 체크
+        st.push(num % 10); //올림값 제외한 값만 스택에 저장
     }
-    if (num >= 10) //마지막 올림 확인
+    while (p1 >= 0) { //a에서 남은 숫자 반영
+        int num = (a[p1--] - '0'); //a의 해당 자릿수 값
+        if (carry) //전 자릿수에서 올림되었다면
+            num += 1;
+        carry = num / 10;  //값이 10을 넘으면 올림변수 체크
+        st.push(num % 10); //올림값 제외한 값만 스택에 저장
+    }
+    while (p2 >= 0) { //b에서 남은 숫자 반영
+        int num = (b[p2--] - '0'); //b의 해당 자릿수 값
+        if (carry) //전 자릿수에서 올림되었다면
+            num += 1;
+        carry = num / 10;  //값이 10을 넘으면 올림변수 체크
+        st.push(num % 10); //올림값 제외한 값만 스택에 저장
+    }
+    if (carry) //마지막 올림 확인
         st.push(1);
 
     return st;
