2068139019dde720c80e6bfac68f04539add8407
suabae <bsa0322@ewhain.net>
2021-09-07 16:15:55 +0900

    [우선순위 큐] 9월 7일 (2493) - update

--- 9월 14일 - 우선순위 큐/2493.cpp
+++ 9월 14일 - 우선순위 큐/2493.cpp
@@ -12,11 +12,13 @@ using namespace std;
  * [문제풀이]
  * 스택에 탑의 높이가 아닌 인덱스를 넣어야 함을 파악해 푸는 문제
  * 항상 스택의 top에 있는 인덱스의 탑 높이가 낮아야 함 (자신보다 높은 탑 중 가장 왼쪽에 있는 탑이 신호 수신하기 때문)
- * 따라서 스택의 top에 있는 인덱스의 탑 높이가 현재 탑 높이와 같거나 낮다면 pop
- * 앞의 과정이 끝난 후 스택이 비어있지 않다면 top에 있는 인덱스가 현재 탑의 신호를 수신하는 탑임
+ * 1. (1 ~ n) 따라서 스택의 top에 있는 인덱스의 탑 높이가 현재 탑 높이와 같거나 낮다면 pop
+ * 1. (1 ~ n) 앞의 과정이 끝난 후 스택이 비어있지 않다면 top에 있는 인덱스가 현재 탑의 신호를 수신하는 탑임
+ * 2. (n ~ 1) 따라서 스택의 top에 있는 인덱스의 탑 높이가 현재 탑 높이보다 낮다면 현재 탑이 수신탑
  */
 
-vector<int> transTop(int n, vector<int> &num) {
+//1 ~ n 돌면서 수신탑 찾기
+vector<int> transTop1(int n, vector<int> &num) {
     stack<int> st;
     vector<int> ans(n + 1, 0); //수신 탑 저장
 
@@ -24,7 +26,23 @@ vector<int> transTop(int n, vector<int> &num) {
         while (!st.empty() && num[st.top()] <= num[i]) { //현재 탑 높이보다 st.top() 인덱스의 탑 높이가 같거나 낮다면 pop
             st.pop();
         }
-        if(!st.empty()) ans[i] = st.top(); //스택이 비어있지 않다면 스택의 top에 저장된 인덱스가 현재 탑의 신호 수신탑
+        if (!st.empty()) ans[i] = st.top(); //스택이 비어있지 않다면 스택의 top에 저장된 인덱스가 현재 탑의 신호 수신탑
+        st.push(i); //현재 탑 인덱스 스택에 push
+    }
+
+    return ans;
+}
+
+//n ~ 1 돌면서 수신탑 찾기
+vector<int> transTop2(int n, vector<int> &num) {
+    stack<int> st;
+    vector<int> ans(n + 1, 0); //수신 탑 저장
+
+    for (int i = n; i >= 1; i--) {
+        while (!st.empty() && num[st.top()] < num[i]) { //현재 탑 높이보다 st.top() 인덱스의 탑 높이가 낮다면 현재 탑이 수신탑
+            ans[st.top()] = i;
+            st.pop(); //수신탑이 정해졌으므로 pop
+        }
         st.push(i); //현재 탑 인덱스 스택에 push
     }
 
@@ -42,7 +60,8 @@ int main() {
         cin >> num[i];
 
     //연산
-    vector<int> ans = transTop(n, num);
+    //vector<int> ans = transTop1(n, num);
+    vector<int> ans = transTop2(n, num);
 
     //출력
     for (int i = 1; i <= n; i++) {
