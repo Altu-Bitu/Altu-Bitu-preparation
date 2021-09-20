9eec7eed8121bd19b71c1a1fe469503509fb26d1
suabae <bsa0322@ewhain.net>
2021-09-20 18:42:43 +0900

    [구현&코너케이스 - 4] 9월 20일 - update

--- 9월 21일 - 브루트 포스/20055.cpp
+++ 9월 21일 - 브루트 포스/20055.cpp
@@ -3,7 +3,13 @@
 
 using namespace std;
 
-int zero_power;  //내구도가 0인 벨트 칸의 개수
+struct info { //내구도와 로봇 존재 여부
+    int power;
+    bool is_on;
+};
+
+vector<info> belt;  //컨테이너 벨트 정보(내구도, 로봇 여부)
+int zero_power;     //내구도가 0인 벨트 칸의 개수
 
 int minusPosition(int n, int pos) { //인덱스 감소
     if (--pos >= 0)
@@ -11,27 +17,28 @@ int minusPosition(int n, int pos) { //인덱스 감소
     return n * 2 - 1;
 }
 
-void second(int n, int start, int end, vector<bool> &robot, vector<int> &power) {
-    int idx = end;
-    while(idx != start) { //end - 1 부터 start 까지 검사
-        idx = minusPosition(n, idx);
-        int next = (idx + 1) % (n * 2); //다음 위치
-        if (robot[idx] && !robot[next] && power[next]) { //현재 위치에 로봇이 있고, 다음 위치에 로봇이 없으며 내구성 1이상인 경우 -> 로봇 옮김
-            robot[idx] = false;
-            power[next]--;
+void second(int n, int start, int end) {
+    int cur = end;
+    while (cur != start) { //end - 1 부터 start 까지 검사
+        cur = minusPosition(n, cur);
+        int next = (cur + 1) % (n * 2); //다음 위치
+        if (belt[cur].is_on && !belt[next].is_on &&
+            belt[next].power) { //현재 위치에 로봇이 있고, 다음 위치에 로봇이 없으며 내구성 1이상인 경우 -> 로봇 옮김
+            belt[cur].is_on = false;
+            belt[next].power--;
             if (next != end) //내리는 위치 아닐 경우만
-                robot[next] = true; //로봇 옮기기
-            if (power[next] == 0)
+                belt[next].is_on = true; //로봇 옮기기
+            if (belt[next].power == 0)
                 zero_power++;
         }
     }
 }
 
-void third(int start, vector<bool> &robot, vector<int> &power) {
-    if (power[start]) { //올리는 위치의 내구도가 0이 아니라면 로봇 올림
-        robot[start] = true;
-        power[start]--;
-        if (power[start] == 0)
+void third(int start) {
+    if (belt[start].power) { //올리는 위치의 내구도가 0이 아니라면 로봇 올림
+        belt[start].is_on = true;
+        belt[start].power--;
+        if (belt[start].power == 0)
             zero_power++;
     }
 }
@@ -46,9 +53,9 @@ void third(int start, vector<bool> &robot, vector<int> &power) {
  *
  * [문제 풀이]
  * 벨트의 로봇을 옮기는 게 아니라, 올리는 위치와 내리는 위치의 인덱스 값을 바꿔서 접근
- * 필요한 컨테이너: 로봇의 존재여부 체크 배열, 내구도 저장한 배열
+ * 필요한 컨테이너: 내구도와 로봇의 존재여부를 체크할 배열
  * 1번: 벨트의 방향이 오른쪽이므로, 벨트 한 칸 회전 시 올리는 위치와 내리는 위치의 인덱스 값은 하나 감소
- * 2번: 가장 먼저 올라간 로봇부터 고려해야 하므로 내리는 위치 - 1 부터 올리는 위치 + 1까지 검사 -> 로봇 옮기는 거 가능하면 존재여부 체크하고 내구도 감소
+ * 2번: 가장 먼저 올라간 로봇부터 고려해야 하므로 (내리는 위치 - 1)부터 (올리는 위치)까지 검사 -> 로봇 옮기는 거 가능하면 존재여부 체크하고 내구도 감소
  * 3번: 올리는 위치 칸 내구도 0이 아니라면 해당 칸 로봇 존재 여부 체크 + 내구도 감소
  */
 
@@ -60,30 +67,28 @@ int main() {
 
     //입력
     cin >> n >> k;
-
-    vector<bool> robot(n * 2, false); //로봇의 존재여부 체크
-    vector<int> power(n * 2, 0);      //내구도 저장
+    belt.assign(n * 2, {0, false});
     for (int i = 0; i < n * 2; i++)
-        cin >> power[i];
+        cin >> belt[i].power;
 
     //연산
-    int start = 0;   //올리는 위치
-    int end = n - 1; //내리는 위치
-    int ans = 0;     //단계 수
+    int start = 0;    //올리는 위치
+    int end = n - 1;  //내리는 위치
+    int step = 0;     //단계 수
     while (true) {
         start = minusPosition(n, start);
         end = minusPosition(n, end);
-        if (robot[end]) //로봇이 내리는 위치에 있다면 내리기
-            robot[end] = false;
-        second(n, start, end, robot, power);
-        third(start, robot, power);
-        ans++;
+        if (belt[end].is_on) //벨트 회전 후, 로봇이 내리는 위치에 있다면 내리기
+            belt[end].is_on = false;
+        second(n, start, end);
+        third(start);
+        step++;
         if (zero_power >= k)
             break;
     }
 
     //출력
-    cout << ans << '\n';
+    cout << step << '\n';
 
     return 0;
 }
--- 9월 21일 - 브루트 포스/2108.cpp
+++ 9월 21일 - 브루트 포스/2108.cpp
@@ -8,16 +8,16 @@ using namespace std;
 const int SIZE = 4000; //양수의 최댓값
 
 int calcMode(vector<int> &frequency) {
-    int max = 0;  //최빈값의 빈도수
+    int max_value = 0;  //최빈값의 빈도수
     for (int i = 0; i <= 8000; i++) { //최빈값의 빈도수 구하기
-        if (frequency[i] > max)
-            max = frequency[i];
+        if (frequency[i] > max_value)
+            max_value = frequency[i];
     }
     //두 번째로 작은 최빈값 구하기
     int cnt = 0;  //최빈값 개수
     int mode = 0; //최빈값
     for (int i = 0; i <= 8000; i++) {
-        if (frequency[i] == max) {
+        if (frequency[i] == max_value) {
             mode = i - SIZE; //SIZE를 더한 값으로 인덱스에 저장했었으므로 실제 값은 SIZE를 빼주어야 함
             cnt++;
             if (cnt == 2) //두 번째로 작은 최빈값
@@ -44,25 +44,25 @@ int main() {
     vector<int> num(n);
     vector<int> frequency(SIZE * 2 + 1, 0); //인덱스는 입력 수 + 4000의 값. 따라서 0 ~ 8000. 각 인덱스에 해당 수가 얼마나 나왔는지 저장
 
-    double sum = 0;             //평균 구하기 위한 합 변수
-    int max = (-1) * SIZE - 1;  //가장 큰 값
-    int min = SIZE + 1;         //가장 작은 값
+    double sum = 0;                   //평균 구하기 위한 합 변수
+    int max_value = (-1) * SIZE - 1;  //가장 큰 값
+    int min_value = SIZE + 1;         //가장 작은 값
     for (int i = 0; i < n; i++) {
         cin >> num[i];
         sum += (double) num[i];
         frequency[num[i] + SIZE]++;
-        if (max < num[i]) max = num[i];
-        if (min > num[i]) min = num[i];
+        if (max_value < num[i]) max_value = num[i];
+        if (min_value > num[i]) min_value = num[i];
     }
 
     //연산
     sort(num.begin(), num.end());
 
     //출력
-    cout << round(sum / n) << '\n';    //산술평균
-    cout << num[n / 2] << '\n';        //중앙값
+    cout << round(sum / n) << '\n';           //산술평균
+    cout << num[n / 2] << '\n';               //중앙값
     cout << calcMode(frequency) << '\n';   //최빈값
-    cout << max - min << '\n';         //범위
+    cout << max_value - min_value << '\n';    //범위
 
     return 0;
 }
