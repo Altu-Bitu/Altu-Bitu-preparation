1bf294707a9916a1b8d611c91a9dee3beab00421
suabae <bsa0322@ewhain.net>
2021-09-26 15:42:06 +0900

    [백트래킹] 9월 26일 (과제 4개) - update

--- 9월 24일 - 백트래킹/15665.cpp
+++ 9월 24일 - 백트래킹/15665.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -7,9 +8,7 @@ const int SIZE = 7; //배열 크기
 const int RANGE = 10000; //주어지는 수 범위
 
 int n, m, leng; //leng: 서로 다른 수의 개수
-int num[SIZE];
-int ans[SIZE];
-int check[RANGE];
+int num[SIZE], ans[SIZE];
 
 void backtracking(int cnt) {
     if (cnt == m) { //길이 m이라면 (기저조건)
@@ -18,37 +17,33 @@ void backtracking(int cnt) {
         cout << '\n';
         return;
     }
-    for (int i = 0; i < leng; i++) {
-        if (check[num[i]] >= 0) { //해당 수 사용 가능하다면
-            check[num[i]]--;
-            ans[cnt] = num[i];
-            backtracking(cnt + 1);
-            check[num[i]]++;
-        }
+    for (int i = 0; i < leng; i++) { //중복 수 모두 제거되었으므로 따로 체크할 필요 없이 수열을 만든다
+        ans[cnt] = num[i];
+        backtracking(cnt + 1);
     }
 }
 
 /**
- * [백트래킹 풀이] (180ms)
+ * [백트래킹 풀이] (176ms)
  * 조건: 같은 수 여러 번 사용 가능, 수열은 중복되면 안됨, 사전 순으로 출력
- * '같은 수 여러 번'
- * -> 그래도 구하려는 수열 길이가 m이므로 각 수는 '최대 m번' 까지밖에 사용 못함.
- * -> check 배열을 미리 m으로 초기화(사용할 수 있는 횟수)하고 사용할 때마다 감소시키자!
- * -> check 배열이 0 미만이면 사용 불가 (가지치기)
- * '수열 중복되면 안됨' 조건도 위 과정에서 만족
+ * '같은 수 여러 번' -> 완전탐색
+ * '수열 중복되면 안됨'
+ * -> 현재 중복으로 들어오는 수가 존재하여, 그냥 완전탐색을 진행하면 중복 수열이 생김
+ * -> 따라서, 중복으로 들어오는 수를 제거하여 다른 배열에 저장한 후, 완전탐색을 하자!
  * '사전 순으로 출력' (N과 M(1)과 동일)
  */
 
 int main() {
+    vector<bool> check(RANGE, false); //중복 수 제거하며 받기 위한 체크 배열
     int a;
 
-    //입력
+    //입력 (중복 제거하며 num 배열에 저장하기)
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
         cin >> a;
-        if (check[a] == 0) //check가 초기화 되기 전. 즉, 처음 나온 수라면
+        if (!check[a]) //처음 나온 수라면
             num[leng++] = a;
-        check[a] = m; //check 초기화 (a를 m만큼 사용할 수 있다는 뜻)
+        check[a] = true;
     }
 
     //연산
