270553649634eafbc35d8b253b28dc20c6d02357
HyunJi <hyunjilim98@gmail.com>
2021-09-29 13:56:00 +0900

    [분할 정복] 9월 29일 (라이브 코딩) - update

--- 10월 5일 - 분할 정복/10830.cpp
+++ 10월 5일 - 분할 정복/10830.cpp
@@ -9,7 +9,7 @@ matrix a;
 
 //Combine : 행렬곱 결과 리턴
 matrix combine(int n, matrix m1, matrix m2) {
-    matrix result(n, vector<int>(n));
+    matrix result(n, vector<int>(n, 0));
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
