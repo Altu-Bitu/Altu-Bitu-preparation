9b721a1cd913983502a59a486c2ded8bafc79771
iw0406 <hyunjilim98@gmail.com>
2021-10-01 13:24:12 +0900

    [투 포인터] 10월 1일 (라이브 코딩)

---
+++ 10월 12일 - 투 포인터/11659.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m, num;
+
+    //입력
+    cin >> n >> m;
+    vector<int> sum(n + 1, 0);
+    for (int i = 1; i <= n; i++) { //인덱스를 1부터 시작하면 i=0인 경우를 따로 처리할 필요 없음
+        cin >> num;
+        sum[i] = sum[i - 1] + num; //첫번째 수부터 i번째 수까지의 누적합
+    }
+
+    for (int i = 0; i < m; i++) {
+        int s, e;
+        cin >> s >> e;
+
+        //출력
+        cout << sum[e] - sum[s - 1] << '\n';
+    }
+}
