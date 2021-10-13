80a924b0895da3bb211cb78d67f2fdfc44f44671
iw0406 <hyunjilim98@gmail.com>
2021-10-13 17:12:37 +0900

    [투 포인터] 10월 13일 (과제 4개) - update

--- 10월 12일 - 투 포인터/20922.cpp
+++ 10월 12일 - 투 포인터/20922.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <set>
 
 using namespace std;
 const int MAX = 100000;
@@ -9,24 +8,15 @@ int findLen(vector<int> &arr, int n, int k) {
     vector<int> cnt(MAX + 1, 0); //각 원소 등장 횟수
 
     int ans = 0, left = 0, right = 0;
-    set<int> s; //k번보다 많이 등장한 원소
-    cnt[arr[0]]++; //초기값
 
     //left는 항상 right의 왼쪽 또는 같은 위치에 있어야 함
     while (left <= right && right < n) {
-        if (s.empty()) { //k번 보다 많이 등장한 원소가 없다면 범위 늘리기
+        if (cnt[arr[right]] + 1 <= k) { //현재 right가 가리키는 원소를 포함해도 등장 횟수가 k번 이하
+            cnt[arr[right]]++;
             ans = max(ans, (right - left) + 1);
             right++;
-            if (right == n)
-                break;
-
-            cnt[arr[right]]++;
-            if (cnt[arr[right]] > k) //k번 초과
-                s.insert(arr[right]);
-        } else { //k번 보다 많이 등장한 원소가 있다면 범위 줄이기
+        } else { //현재 right가 가리키는 원소를 포함하면 등장 횟수가 k번을 초과
             cnt[arr[left]]--;
-            if (cnt[arr[left]] == k) //원래 등장 횟수가 k번을 초과했던 원소
-                s.erase(arr[left]);
             left++;
         }
     }
@@ -36,7 +26,7 @@ int findLen(vector<int> &arr, int n, int k) {
 /**
  * 1. 최장 연속 부분 수열의 '길이'를 구해야 함 -> 구간 내의 모든 원소를 고려하기 때문에 한쪽에서 시작하는 투 포인터
  * 2. 각 원소의 최댓값이 100,000이기 때문에 배열로 등장 횟수 체크할 수 있음
- * 3. 매번 배열을 돌며 k번 이상 등장한 원소 찾기? -> 비효율적! k번 이상 등장한 원소를 set에 넣자
+ * 3. right를 옮기고 바로 원소를 추가했던 기존 방법과 달리, 일단 포인터만 옮겨놓고 원소를 추가해도 되는지 확인
  */
 int main() {
     int n, k;
