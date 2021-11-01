5026cfc396a08e59a9b9b1443baa483a9c6e40a0
suabae <bsa0322@ewhain.net>
2021-11-01 19:49:36 +0900

    [DFS & BFS] 11월 1일 (과제 4개) - update

--- 10월 15일 - DFS & BFS/1697.cpp
+++ 10월 15일 - DFS & BFS/1697.cpp
@@ -2,31 +2,29 @@
 #include <queue>
 
 using namespace std;
-typedef pair<int, int> ci;
 const int SIZE = 100000;
 
 int bfs(int n, int k) {
-    vector<bool> visited(SIZE + 1, false);
-    queue<ci> q; //큐에 좌표값과 시간을 pair로 저장
+    vector<int> check(SIZE + 1, 0);
+    queue<int> q; //큐에 좌표값 저장
     int ans = 0;
 
-    visited[n] = true; //시작 노드 방문체크
-    q.push(ci(n, 0)); //시작 노드 초기화
+    check[n] = 1; //시작 노드 방문체크 + 시간 초기화
+    q.push(n); //시작 노드 초기화
     while (!q.empty()) {
-        int x = q.front().first; //현재 탐색하려는 위치
-        int t = q.front().second;
+        int x = q.front(); //현재 탐색하려는 위치
         q.pop();
 
-        if (x == k) { //찾으면 탐색 종료
-            ans = t;
+        if (x == k) { //동생 찾으면 탐색 종료. 시작 위치를 1로 초기화 했기 때문에 1을 빼줌
+            ans = check[x] - 1;
             break;
         }
 
         vector<int> child = {x - 1, x + 1, x * 2}; //자식 노드
         for (int i = 0; i < 3; i++) {
-            if (child[i] >= 0 && child[i] <= SIZE && !visited[child[i]]) { //범위 내에 있고, 첫 방문이라면
-                visited[child[i]] = true;
-                q.push(ci(child[i], t + 1));
+            if (child[i] >= 0 && child[i] <= SIZE && !check[child[i]]) { //범위 내에 있고, 첫 방문이라면
+                check[child[i]] = check[x] + 1; //시간 증가
+                q.push(child[i]);
             }
         }
     }
