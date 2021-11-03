d1c03c1a3b7ca0095cf5c2151bde9cb6887c6bb1
bsa0322 <bsa0322@ewhain.net>
2021-11-04 03:55:17 +0900

    [구현&코너케이스 - 8] 11월 4일 - update

--- 11월 2일 - 최단 경로/1063.cpp
+++ 11월 2일 - 최단 경로/1063.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 typedef pair<int, int> ci;
 const int SIZE = 8;
+
+map<string, int> direction = {{"L",  0},  //상
+                              {"R",  1},  //하
+                              {"B",  2},  //좌
+                              {"T",  3},  //우
+                              {"LT", 4},  //우상향
+                              {"LB", 5},  //좌상향
+                              {"RT", 6},  //우하향
+                              {"RB", 7}}; //좌하향
 
 string pointToString(ci point) {
     string ans;
@@ -11,22 +21,11 @@ string pointToString(ci point) {
     return ans;
 }
 
-int direction(string move) {
-    if (move == "L") return 0;
-    if (move == "R") return 1;
-    if (move == "B") return 2;
-    if (move == "T") return 3;
-    if (move == "LT") return 4;
-    if (move == "LB") return 5;
-    if (move == "RT") return 6;
-    if (move == "RB") return 7;
-}
-
 pair<ci, ci> moveKing(string move, ci point_k, ci point_r) {
     //상, 하, 좌, 우, 우상향, 좌상향, 우하향, 좌하향
     int dr[8] = {-1, 1, 0, 0, -1, -1, 1, 1};
     int dc[8] = {0, 0, -1, 1, 1, -1, 1, -1};
-    int d = direction(move); //방향 인덱스 저장
+    int d = direction[move]; //방향 인덱스 저장
 
     int king_nr = point_k.first + dr[d];
     int king_nc = point_k.second + dc[d];
@@ -53,6 +52,7 @@ pair<ci, ci> moveKing(string move, ci point_k, ci point_r) {
  * RT:오위 -> 오아   LT:왼위 -> 오위   RB:오아 -> 왼아   LB:왼아 -> 왼위
  *
  * => 바뀐 방향대로 입력에 따라 킹과 돌을 움직임
+ * 방향 구현을 효율적으로 하기 위해 탐색 시간복잡도가 logN인 map 사용
  *
  * [경우의 수]
  * 1. 킹이 범위를 벗어나는 경우
