739e0f3f72996a98a4e6a65e5c1caa69fc8f730b
iw0406 <hyunjilim98@gmail.com>
2021-11-07 14:39:50 +0900

    [동적 계획법과 최단 거리 역추적] 11월 7일 (과제 2개) - update

--- 11월 9일 - 동적 계획법과 최단 거리 역추적/16964.cpp
+++ 11월 9일 - 동적 계획법과 최단 거리 역추적/16964.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <queue>
 #include <vector>
 #include <algorithm>
 
--- 11월 9일 - 동적 계획법과 최단 거리 역추적/1719.cpp
+++ 11월 9일 - 동적 계획법과 최단 거리 역추적/1719.cpp
@@ -4,9 +4,7 @@
 using namespace std;
 const int INF = 1e5 * 2; //최대 n-1개의 간선을 지나게 됨
 
-vector<vector<int>> graph, table;
-
-void floydWarshall(int n) {
+void floydWarshall(int n, vector<vector<int>> &graph, vector<vector<int>> &table) {
     for (int k = 1; k <= n; k++) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
@@ -36,16 +34,15 @@ int main() {
 
     //입력
     cin >> n >> m;
-    graph.assign(n + 1, vector<int>(n + 1, INF));
-    table.assign(n + 1, vector<int>(n + 1, 0));
+    vector<vector<int>> graph(n+1, vector<int>(n+1, INF));
+    vector<vector<int>> table(n+1, vector<int>(n+1, 0));
     for (int i = 1; i <= n; i++)
         graph[i][i] = 0;
 
     while (m--) { //무방향 그래프
         cin >> s >> d >> c;
         //간선 정보
-        graph[s][d] = c;
-        graph[d][s] = c;
+        graph[s][d] = graph[d][s] = c;
 
         //경로 정보
         table[s][d] = d;
@@ -53,7 +50,7 @@ int main() {
     }
 
     //연산
-    floydWarshall(n);
+    floydWarshall(n, graph, table);
 
     //출력
     for (int i = 1; i <= n; i++) {
